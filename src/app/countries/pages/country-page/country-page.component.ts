a62ade1e179771eaaba1343373b0a1a3af447878
Juan Gomez <gomezsandovalpablo@gmail.com>
2024-06-12 16:13:58 -0700

    single Country page working

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -10,6 +11,7 @@ import { switchMap } from 'rxjs';
 })
 export class CountryPageComponent implements OnInit {
 
+  public country?: Country;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -20,16 +22,11 @@ export class CountryPageComponent implements OnInit {
   ngOnInit(): void {
     this.activatedRoute.params
       .pipe(
-        switchMap( ({ id }) => this.countriesService.searchCountryByAlphaCode(id) )
+        switchMap(({ id }) => this.countriesService.searchCountryByAlphaCode(id))
       )
       .subscribe(country => {
-
-        if (!country) {
-          return this.router.navigateByUrl('');
-        }
-
-        console.log('We have a country!')
-        return;
+        if (!country) return this.router.navigateByUrl('');
+        return this.country = country;
       });
   }
 
